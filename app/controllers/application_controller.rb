d942ddd8b1300c6ae8f08bf755957128d916ec99
suncycle <55820659@qq.com>
2015-08-07 12:51:21 +0000

    hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello rails!"
+    render text: "hello rails!!!!!"
   end
 end
